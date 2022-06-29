699a508560c8460c45de58dcc51bc3942606e728
Débora Serra <debora.r.serra@gmail.com>
2022-06-29 13:40:34 -0300

    fix: fix req 1

--- services/productService.js
+++ services/productService.js
@@ -9,7 +9,7 @@ const productService = {
   findById: async (id) => {
     const product = await productModel.findById(id);
     if (product.length === 0) return { code: 404, message: 'Product not found' };
-    return { code: 200, product };
+    return { code: 200, product: product[0] };
   },
 }
 
