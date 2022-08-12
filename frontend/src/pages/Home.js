b0cfb22974cf317db111dcf11d2d6ac179eccee6
debora <debora.r.serra@gmail.com>
2022-08-11 21:04:41 -0300

    style: add style to the home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from '../styles/Home.module.css';
 
 const Home = () => {
@@ -7,6 +8,16 @@ const Home = () => {
       <h2 className={ style.title }>Welcome</h2>
       <p className={ style.p }>To see or add a product, click the products link</p>
       <p className={ style.p }>To see or add a sale, click the sales link</p>
+      <section className={ style.sect }>
+        <section className={ style.link_sect }>
+          <h3>To add a new product</h3>
+          <Link className={ style.link } to="/products/new">Click Here</Link>
+        </section>
+        <section className={ style.link_sect }>
+          <h3>To add a new sale</h3>
+          <Link className={ style.link } to="/products/new">Click Here</Link>
+        </section>
+      </section>
     </section>
   )
 }
